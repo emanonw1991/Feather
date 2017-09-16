6ef74ba57f8e851714f308986cc04e0a266bcbe4
emanon <emanonw@163.com>
2017-09-16 11:58:23 +0800

    完成遍历省市县三级列表的功能。

--- app/src/main/java/com/vivo/emanon/feather/db/Province.java
+++ app/src/main/java/com/vivo/emanon/feather/db/Province.java
@@ -10,7 +10,7 @@ public class Province extends DataSupport {
 
     private int id;
     private String provinceName;
-    private String provinceCode;
+    private int provinceCode;
 
     public int getId() {
         return id;
@@ -28,11 +28,11 @@ public class Province extends DataSupport {
         this.provinceName = provinceName;
     }
 
-    public String getProvinceCode() {
+    public int getProvinceCode() {
         return provinceCode;
     }
 
-    public void setProvinceCode(String provinceCode) {
+    public void setProvinceCode(int provinceCode) {
         this.provinceCode = provinceCode;
     }
 }
